ce794a9515ac746cc62aa07fda38250ef9492ccf
Conor Grace <20093293@mail.wit.ie>
2023-12-08 01:14:58 +0000

    Logout functionality added

--- app/src/main/java/ie/setu/familytrip/LoginActivity.kt
+++ app/src/main/java/ie/setu/familytrip/LoginActivity.kt
@@ -1,12 +1,11 @@
 package ie.setu.familytrip
-
+import android.util.Log
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.media3.common.util.Log
 import com.google.firebase.auth.FirebaseAuth
 
 
