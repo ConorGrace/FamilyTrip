e8bb27a3d83a61e185b91a925af5f795aa8a2da4
Conor Grace <20093293@mail.wit.ie>
2023-12-26 14:33:00 +0000

    added Firestore functionality to my application

--- app/src/main/java/ie/setu/familytrip/PostsActivity.kt
+++ app/src/main/java/ie/setu/familytrip/PostsActivity.kt
@@ -3,13 +3,30 @@ package ie.setu.familytrip
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.firestore.FirebaseFirestore
 
+private const val TAG = "PostsActivity"
 class PostsActivity : AppCompatActivity() {
+
+    private lateinit var firestoreDb: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
+
+        firestoreDb = FirebaseFirestore.getInstance()
+        val postsReference = firestoreDb.collection("trips")
+        postsReference.addSnapshotListener { snapshot, exception ->
+            if (exception != null || snapshot == null) {
+                Log.e(TAG, "Exception when querying posts", exception)
+                return@addSnapshotListener
+            }
+            for (document in snapshot.documents) {
+                Log.i(TAG, "Document ${document.id}: ${document.data}")
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
